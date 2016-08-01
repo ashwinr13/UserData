a61c9a0d6440eb72241b7c84a009369908f8d724
ashwinr13 <ashwinravindran993@gmail.com>
2016-07-31 18:57:54 -0700

    commit email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 	validates :age, :numericality => {:message => "should be a number"}
-	validates :email, :presence => true, :uniqueness => true
+	validates :email, :presence => true, :uniqueness => true, format: {with: VALID_EMAIL_REGEX}
 end
